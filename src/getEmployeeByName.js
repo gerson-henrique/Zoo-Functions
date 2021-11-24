4db1ce3bb28657d7fb4e486eb64c128b9049a85c
Gero <gersonhoa@gmail.com>
2021-11-24 19:39:29 -0300

    solução 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof (employeeName) === 'undefined') return {};
+  const guys = employees.filter((p) => {
+    console.log('');
+    return (p.firstName === employeeName || p.lastName === employeeName);
+  });
+  return guys[0];
 }
 
 module.exports = getEmployeeByName;

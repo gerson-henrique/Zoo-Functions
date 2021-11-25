40d471fda9c99321fc4bbc3939b3e635c679714b
Gero <gersonhoa@gmail.com>
2021-11-25 15:46:07 -0300

    resolução 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,61 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const sexSorted = (options) => {
+  const windRose = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((membro) => {
+    const sexFiltered = membro.residents.filter((res) => res.sex === options.sex);
+    windRose[membro.location].push({ [membro.name]: sexFiltered.map((sext) => sext.name).sort() });
+  });
+  return windRose;
+};
+
+const onlySorted = (options) => {
+  const windRose = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((a) => {
+    windRose[a.location].push({ [a.name]: a.residents.map((all) => all.name).sort() });
+  });
+  return windRose;
+};
+
+const onlySex = (options) => {
+  const windRose = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((membro) => {
+    const sexFiltered = membro.residents.filter((res) => res.sex === options.sex);
+    windRose[membro.location].push({ [membro.name]: sexFiltered.map((sext) => sext.name) });
+  });
+  return windRose;
+};
+
+const namesOnly = (options) => {
+  const windRose = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((membro) => {
+    windRose[membro.location].push({ [membro.name]: membro.residents.map((all) => all.name) });
+  });
+  return windRose;
+};
+
+function optionChooser(options) {
+  if (Object.prototype.hasOwnProperty.call(options, 'sorted')) {
+    if (Object.prototype.hasOwnProperty.call(options, 'sex')) {
+      return sexSorted(options);
+    }
+    return onlySorted(options);
+  }
+  if (Object.prototype.hasOwnProperty.call(options, 'sex')) {
+    return onlySex(options);
+  }
+  return namesOnly(options);
 }
 
+function getAnimalMap(options) {
+  const windRose = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((family) => {
+    windRose[family.location].push(family.name);
+  });
+  if (typeof (options) === 'undefined') return windRose;
+  if (!options.includeNames) return windRose;
+  const animalArr = optionChooser(options);
+  return animalArr;
+}
 module.exports = getAnimalMap;

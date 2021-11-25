803737b41868bb102b9f6106d642464c747c2e19
Gero <gersonhoa@gmail.com>
2021-11-24 21:01:52 -0300

    solução 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) => employees.some((person) => person.managers.includes(id));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colaborator = employees.filter((employer) => employer.managers.includes(managerId));
+  const employersList = colaborator.map((personName) => {
+    console.log('');
+    return `${personName.firstName} ${personName.lastName}`;
+  });
+  return employersList;
 }
-
 module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,5 +2,5 @@ const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   return (ids.length === 0 ? [] : species.filter((a) => ids.includes(a.id)));
-}
+}// a = animals;
 module.exports = getSpeciesByIds;

a43d069b5ffc9786b012f2cc6f8e612ac102a7ff
Gero <gersonhoa@gmail.com>
2022-04-08 12:24:48 -0300

    reupload : js zoo function

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,8 @@
 const { species } = require('../data/zoo_data');
-
 function getAnimalsOlderThan(animal, age) {
   const animalSpecies = species.filter((s) => (s.name) === animal);
   const everyAnimal = animalSpecies.map((member) => member.residents);
   const animalLenght = everyAnimal[0].every((anim) => anim.age > age);
   return animalLenght;
 }// s = species
-
 module.exports = getAnimalsOlderThan;
